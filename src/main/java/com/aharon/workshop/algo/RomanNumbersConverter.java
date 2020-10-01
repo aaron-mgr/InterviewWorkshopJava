75b216cb12d02676c0c5daddf592c21f9eae44ff
Anonymous <>
2020-10-01 13:42:45 +0300

    move classes by type

--- src/main/java/com/aharon/workshop/algo/RomanNumbersConverter.java
+++ src/main/java/com/aharon/workshop/algo/RomanNumbersConverter.java
@@ -9,7 +9,6 @@ import static org.junit.Assert.*;
 /**
  * Convert roman numbers to decimal
  */
-
 public class RomanNumbersConverter {
 
 
@@ -18,59 +17,63 @@ public class RomanNumbersConverter {
     @Test
     public void test1() {
         assertEquals(romanNumbers.convertRomanNumbers("XLI"), 41);
+        assertEquals(romanNumbers.convertRomanNumbers("xLi"), 41);
     }
     @Test
     public void test2() {
         assertEquals(romanNumbers.convertRomanNumbers("MLCXVI"), 1066);
-    }@Test
+    }
+    @Test
     public void test3() {
         assertEquals(romanNumbers.convertRomanNumbers("MMMCMXCIX"), 3999);
-    }@Test
+    }
+    @Test
     public void test4() {
         assertEquals(romanNumbers.convertRomanNumbers("X"), 10);
     }
 
-    private static void testRoman(String roman, int decimal) {
-        int result = romanNumbers.convertRomanNumbers(roman);
-        if (result != decimal) {
-            throw new RuntimeException("Wrong result supposed:" + decimal + " get:" + result);
-        }
-        System.out.println("Input: " + roman + " Result:" + result);
-
-    }
-
-
-    public Map<String, Integer> map = new HashMap<String, Integer>();
-
+    /**
+     * Init - symbols mapping
+     */
+    public Map<Character, Integer> map = new HashMap<Character, Integer>();
     public RomanNumbersConverter() {
-        map.put("I", 1);
-        map.put("X", 10);
-        map.put("V", 5);
-        map.put("L", 50);
-        map.put("C", 100);
-        map.put("D", 500);
-        map.put("M", 1000);
-
-
+        /* support lowcase and capital characters */
+        map.put('I', 1);
+        map.put('X', 10);
+        map.put('V', 5);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        map.put('i', 1);
+        map.put('x', 10);
+        map.put('v', 5);
+        map.put('l', 50);
+        map.put('c', 100);
+        map.put('d', 500);
+        map.put('m', 1000);
     }
 
-    private int convertRomanNumbers(String roman) {
-
-        int result = 0;
+    /**
+     * Runs over numbers form less significant to most significant
+     * If current roman number is less than previous - subtract it
+     * otherwise add it.
+     * @param roman number as string
+     * @return decimal number
+     */
+    private long convertRomanNumbers(String roman) {
+        long result = 0;
         char [] charArr = roman.toCharArray();
         int prev = 0;
-
         for (int i=charArr.length-1 ; i>=0; i--) {
-
             char curr = charArr[i];
-            int currInt = map.get(String.valueOf(curr)); // TODO use character
+            int currInt = map.get(curr);
             if (currInt < prev) {
                 result = result - currInt;
             } else {
                 result = result + currInt;
             }
             prev = currInt;
-
         }
         return result;
     }
